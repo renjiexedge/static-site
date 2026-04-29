6614c99600dff6b54614b00200d947ba989d5f22
renjie <renjie.xedge@gmail.com>
2026-04-29 15:00:55 +0800

    3rd test to connect to supabase

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // This file contains the JavaScript code for the static site.
 // You can add functions to manipulate the DOM, handle events, or perform other client-side operations.
 
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from './@supabase/supabase-js';
 
 const supabaseUrl = 'https://loqujxhskaiqlwurcrdl.supabase.co';
 const supabaseKey = 'sb_publishable_PvUfGDzNIwSwF21GeRVhPw_FxAxy2fC';
